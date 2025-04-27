212fc2ff3bc7e89f5647e0a57633dd1f40d2f849
arvinlstewart <arvinlstewart@gmail.com>
2025-04-27 12:20:41 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -112,7 +112,7 @@ document.getElementById("quizForm")?.addEventListener("submit", (e) => {
 
   console.log("Submitting data:", data);
 
- fetch('https://arvin-proxy.onrender.com/submit', {
+ fetch("/api/submit", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
