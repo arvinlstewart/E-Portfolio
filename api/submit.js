3e89bda3f36997ffa2f948bd03e66bf0b9a62f61
arvinlstewart <arvinlstewart@gmail.com>
2025-04-27 16:08:34 -0400

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // Enable CORS
-  res.setHeader('Access-Control-Allow-Origin', 'https://arvinstewart.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
