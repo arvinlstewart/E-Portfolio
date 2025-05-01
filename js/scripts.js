05f01ddcc2dacc2f50e70f4db96569a222c2954c
arvinlstewart <arvinlstewart@gmail.com>
2025-04-30 23:26:34 -0400

    disable form from JS

--- js/scripts.js
+++ js/scripts.js
@@ -188,7 +188,7 @@ if (quizModal && openQuizBtn && closeQuiz && quizSteps.length > 0) {
 }
 
 // ===== Quiz Form Submission =====
-const quizForm = document.getElementById('quizForm');
+*/*const quizForm = document.getElementById('quizForm');
 if (quizForm) {
   quizForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -218,7 +218,7 @@ if (quizForm) {
     });
   });
 }
-
+*/*
 // ===== Carousel Functionality =====
 const track = document.querySelector('.carousel-track');
 const items = document.querySelectorAll('.carousel-item');
